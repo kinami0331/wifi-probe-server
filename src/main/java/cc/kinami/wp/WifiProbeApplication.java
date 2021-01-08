4f23f7c1c85732fdfeac9fe2c8046c7dc6976bc3
kinami <sunhaonan331@outlook.com>
2021-01-09 00:01:41 +0800

    [初始化] 项目初始化

---
+++ src/main/java/cc/kinami/wp/WifiProbeApplication.java
@@ -0,0 +1,13 @@
+package cc.kinami.wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WifiProbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WifiProbeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cc/kinami/wp/WifiProbeApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.kinami.wp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WifiProbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
